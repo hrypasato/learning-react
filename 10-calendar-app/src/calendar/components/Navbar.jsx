4c47debddfbc4684932c8edb97d70f2c21869c63
Henry Quinde <hrypasato@gmail.com>
2022-10-20 19:07:38 -0500

    complete login and register user

--- 10-calendar-app/src/calendar/components/Navbar.jsx
+++ 10-calendar-app/src/calendar/components/Navbar.jsx
@@ -1,15 +1,25 @@
+import { useAuthStore } from "../../hooks"
+
 export const Navbar = () => {
+
+    const { startLogout, user } = useAuthStore();
+
+
+    const onClickLogout = () => {
+        startLogout();
+    }
+
     return (
         <div className="navbar navbar-dark bg-dark mb-4 px-4">
             <span className="navbar-brand">
                 <i className="fas fa-calendar-alt"></i>
                 &nbsp;
-                Miguel
+                { user.name }
             </span>
-            <button className="btn btn-outline-danger">
-                <i className="fas fa-sign-out-alt">
-                    <span>Salir</span>
-                </i>
+            <button onClick={ onClickLogout } className="btn btn-outline-danger">
+                <i className="fas fa-sign-out-alt"></i>
+                &nbsp; 
+                <span>Salir</span>
             </button>
         </div>
     )
